1e3489b6a5beeb5b337266c8a58ef32c2a291b84
Amanda Miranda <amanda.miranda@ntconsult.com.br>
2025-12-16 22:33:45 -0300

    feat: implementa endpoints de sensores

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/config/jackson/teste.java
@@ -0,0 +1,10 @@
+package com.algaworks.algasensors.device.management.api.config.jackson;
+
+public class teste {
+
+    @Override
+    public String toString() {
+        return super.toString();
+
+    }
+}
